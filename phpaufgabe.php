82e548f0572de691e27e3bd2b0933099b3e47efe
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-16 21:51:48 +0100

    php

---
+++ phpaufgabe.php
@@ -0,0 +1,18 @@
+<?php
+// example code
+function get_zahlen($custom = 0) {
+
+(int) $zahl = 50;
+//checks if its in an integer
+var_dump(is_int($zahl));
+
+
+for ($x = 0; $x < 3; $x++) {
+    //adds 25 % of the $custom
+$zahl=$zahl +  ($custom*0.25);
+}
+echo "$zahl";
+}
+
+functionName(12)
+
